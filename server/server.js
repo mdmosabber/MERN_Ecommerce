7b04871831e578b13b4a3c44805faea2186b4b1c
mdmosabber <33853500+mdmosabber@users.noreply.github.com>
2023-06-16 22:08:12 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,6 +38,14 @@ app.use(cors({
   credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 
 //Router middleware
 readdirSync('./src/routes').map((routeFile) =>  app.use('/api/v1', require(`./src/routes/${routeFile}`)));
